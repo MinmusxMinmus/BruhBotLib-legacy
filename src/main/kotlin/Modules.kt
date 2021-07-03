6902be5678942ae8bf44fda4ec4a03471af8c766
Min <minmusxminmus@hotmail.com>
2021-07-03 22:59:21 +0200

    Moved

---
+++ src/main/kotlin/Modules.kt
@@ -0,0 +1,28 @@
+package remote
+
+import model.Command
+import model.CommandDeclaration
+import java.rmi.Remote
+
+/*
+The objective is to allow easy module creation. Anyone that wants to create modules should just include this library,
+then create an executable JAR that adds the corresponding factory to the registry for RMI. In the bot core, there should
+be a command that allows you to add/remove modules (for example, !module add/remove modulename). The bot then adds or
+removes the module's commands to its command pool (as well as whatever else modules are able to add), and voila!
+ */
+
+/**
+ * This interface serves to define all possible bot modules.
+ *
+ * A module is defined as an independent executable that can be remotely invoked by the bot, offering additional
+ * functionality that is not necessary for basic execution. An example would be a module that only
+ * offers additional commands to the bot.
+ *
+ * Any module must implement [commands], a function which sends information about the commands the module
+ * adds. Some modules may not add commands at all, in which case they would send an empty set. Modules must also
+ * implement [getCommand], a method to obtain a command instance for the given [CommandDeclaration].
+ */
+interface BBModules : Remote {
+    fun commands(): Set<CommandDeclaration>
+    fun getCommand(type: CommandDeclaration): Command
+}
