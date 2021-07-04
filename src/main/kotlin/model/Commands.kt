6acf914fadde0d8b1f12268bba72531c750d22c1
Min <minmusxminmus@hotmail.com>
2021-07-04 12:50:51 +0200

    Split into a new file for ease of reading

--- src/main/kotlin/model/Commands.kt
+++ src/main/kotlin/model/Commands.kt
@@ -4,31 +4,12 @@ import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.User
+import remote.model.CommandPermission
+import remote.model.ExecutionError
+import remote.model.ExecutionEventBase
 import java.io.Serializable
 import java.time.Instant
 import kotlin.reflect.KClass
-
-/**
- * Describes a notable event during command execution. This is used for both normal events and errors / exceptions that
- * might occur, indicated by the subclass.
- */
-abstract class ExecutionEventBase(open val timestamp: Instant, open val info: String) : Serializable
-
-/**
- * Describes a standard event during command execution. This can be as simple as beginning a new loop, finishing processing
- * of a certain task, or anything else worth mentioning.
- */
-data class ExecutionEvent(override val timestamp: Instant,
-                          override val info: String) : ExecutionEventBase(timestamp, info)
-
-/**
- * Describes an error during command execution. Said error can be caused by an exception (caught or uncaught) or it can
- * be caused by a failed condition. The error can be fatal and force to stop command execution, or it can be dealt with
- * using an alternative solution, or even just aknowledged and ignored henceforth.
- */
-data class ExecutionError(override val timestamp: Instant,
-                          override val info: String,
-                          val exception: Exception?) : ExecutionEventBase(timestamp, info)
 
 /**
  * Defines all information required to use its corresponding command. Includes the name of the command, as well as a
---
+++ src/main/kotlin/model/ExecutionEvents.kt
@@ -0,0 +1,24 @@
+package remote.model
+
+import java.io.Serializable
+import java.time.Instant
+
+/**
+ * Describes a notable event during command execution. This is used for both normal events and errors / exceptions that
+ * might occur, indicated by the subclass.
+ */
+abstract class ExecutionEventBase(val timestamp: Instant = Instant.now(), open val info: String) : Serializable
+
+/**
+ * Describes a standard event during command execution. This can be as simple as beginning a new loop, finishing processing
+ * of a certain task, or anything else worth mentioning.
+ */
+data class ExecutionEvent(override val info: String) : ExecutionEventBase(info = info)
+
+/**
+ * Describes an error during command execution. Said error can be caused by an exception (caught or uncaught) or it can
+ * be caused by a failed condition. The error can be fatal and force to stop command execution, or it can be dealt with
+ * using an alternative solution, or even just aknowledged and ignored henceforth.
+ */
+data class ExecutionError(override val info: String,
+                          val exception: Exception?) : ExecutionEventBase(info = info)
