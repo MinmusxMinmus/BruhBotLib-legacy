746e4a470cd80af816d2e3a9c37fabf5a6e6a93d
Min <minmusxminmus@hotmail.com>
2021-07-18 21:35:51 +0200

    fix: add port to registry

--- src/main/kotlin/ModuleLoader.kt
+++ src/main/kotlin/ModuleLoader.kt
@@ -8,7 +8,7 @@ object ModuleLoader {
     fun register(module: BBModule) {
         try {
             val stub = UnicastRemoteObject.exportObject(module, 0) as BBModule
-            val registry = LocateRegistry.getRegistry()
+            val registry = LocateRegistry.getRegistry(1099)
             registry.rebind(module.name(), stub)
         } catch (e: Exception) {
             System.err.println("Module exception:")
