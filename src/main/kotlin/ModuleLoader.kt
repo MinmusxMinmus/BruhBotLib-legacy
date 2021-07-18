145026169a6e6c7044e2dcf53dc1aeb46f5b23e9
Min <minmusxminmus@hotmail.com>
2021-07-18 21:35:33 +0200

    fix: remove security manager

--- src/main/kotlin/ModuleLoader.kt
+++ src/main/kotlin/ModuleLoader.kt
@@ -6,9 +6,6 @@ import java.rmi.server.UnicastRemoteObject
 
 object ModuleLoader {
     fun register(module: BBModule) {
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(SecurityManager())
-        }
         try {
             val stub = UnicastRemoteObject.exportObject(module, 0) as BBModule
             val registry = LocateRegistry.getRegistry()
