f6c2d27480ade5e302048f2d01f4cec4ed989569
Min <minmusxminmus@hotmail.com>
2022-02-06 19:19:21 +0100

    Implement basic parameter types

--- src/main/kotlin/parameters/types/ParameterTypes.kt
+++ src/main/kotlin/parameters/types/ParameterTypes.kt
@@ -4,3 +4,39 @@ interface ParameterType {
     fun getName(): String
     fun getDescription(): String
 }
+
+open class StringParameterType: ParameterType {
+    override fun getName(): String = "String"
+    override fun getDescription(): String = "A string of text."
+}
+
+abstract class KeywordParameterType: StringParameterType() {
+    protected val keywords: MutableCollection<StringParameterType> = mutableSetOf()
+    override fun getName(): String = "Keyword"
+    override fun getDescription(): String = "One of ${keywords.size} different predefined keywords."
+}
+
+open class NumberParameterType: ParameterType {
+    override fun getName(): String = "Number"
+    override fun getDescription(): String = "Any type of number."
+}
+
+open class IntegerParameterType: NumberParameterType() {
+    override fun getName(): String = "Integer"
+    override fun getDescription(): String = "A number with no decimal part."
+}
+
+open class DecimalParameterType: NumberParameterType() {
+    override fun getName(): String = "Decimal"
+    override fun getDescription(): String = "A number with decimal part."
+}
+
+open class BooleanParameterType: ParameterType {
+    override fun getName(): String = "Boolean"
+    override fun getDescription(): String = "A true/false value."
+}
+
+class WildcardParameterType: ParameterType {
+    override fun getName(): String = "Anything"
+    override fun getDescription(): String = "A parameter that can be anything"
+}
